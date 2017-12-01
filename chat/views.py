7a069275a3a2dc7c2bd657896629c42cc762965d
ersul4ik <ersul4ik@mail.ru>
2017-12-01 12:51:39 +0600

    Return last commit

--- chat/views.py
+++ chat/views.py
@@ -106,9 +106,6 @@ def message_create(request):
 def messages_get(request):
     dialog = get_object_or_404(Dialog, Q(client=request.user) | Q(manager=request.user), is_active=True)
     context = ''
-    for s in dialog.messages.filter(seen=False):
-        if request.user != s.sender:
-            context += render_to_string('message_list.html', locals())
     for m in dialog.messages.filter(read=False):
         if request.user != m.sender:
             context += render_to_string('receive.html', locals())
--- static/static_files/js/chief.js
+++ static/static_files/js/chief.js
@@ -2,10 +2,6 @@ $(document).ready(function () {
     if ($('#chat-form')) {
         get_message()
     }
-    else{
-        get_message_seen()
-    }
-
 });
 
 $('#chat-form').on('submit', function (event) {
@@ -28,26 +24,6 @@ $('#chat-form').on('submit', function (event) {
     return false
 });
 
-function get_message_seen() {
-    $.ajax({
-        url: "/messages/get/",
-        type: "GET",
-        async: false
-    })
-        .done(function (data) {
-            $('#message-receive').append(data);
-            if (data) {
-                read_message();
-            }
-        })
-        .fail(function (data) {
-            console.log('error receive');
-        })
-        .always(
-            setTimeout(get_message, 8000)
-        );
-}
-
 function get_message() {
     $.ajax({
         url: "/messages/get/",
@@ -55,7 +31,6 @@ function get_message() {
         async: false
     })
         .done(function (data) {
-            $('#message-receive').append(data);
             $('.msg_container_base').append(data);
             var chatlist = document.getElementById('msg-list-div');
             chatlist.scrollTop = chatlist.scrollHeight;
@@ -78,35 +53,3 @@ function read_message() {
         async: false
     })
 }
-
-
-//
-// function get_message_list() {
-//     $.ajax({
-//         url: "/messages/list/",
-//         type: "GET",
-//         async: false
-//     })
-//         .done(function (data) {
-//             $('.not-view').append(data);
-//             if(data){
-//                 seen_message();
-//             }
-//             console.log('ok')
-//         })
-//         .fail(function (data) {
-//             console.log('Message not receive');
-//         })
-//         .always(
-//             setTimeout(get_message_list, 6000)
-//         );
-// }
-
-
-// function seen_message() {
-//     $.ajax({
-//         url: "/messages/seen/",
-//         type: "POST",
-//         async: false
-//     })
-// }
