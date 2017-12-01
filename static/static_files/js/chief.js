a35125a1fb55dea4176a15e20a40a5dde8118799
ersul4ik <ersul4ik@mail.ru>
2017-12-01 11:19:06 +0600

    sending a mess on the enter

--- static/static_files/js/chief.js
+++ static/static_files/js/chief.js
@@ -2,27 +2,40 @@ $(document).ready(function () {
     if ($('#chat-form')) {
         get_message()
     }
+
+    $(".chat_input").keydown(
+        function (event) {
+            if (event.keyCode == 13) {
+                sent();
+            }
+        }
+    );
+
+
 });
 
-$('#chat-form').on('submit', function (event) {
-    event.preventDefault();
-    $.ajax({
-        url: $(this).attr('action'),
-        type: $(this).attr('method'),
-        data: $(this).serialize()
-    })
-        .done(
-            function (data) {
-                $('.name-input').remove();
-                $('.chat_input').val('');
-                $('.msg_container_base').append(data);
+function sent() {
+    $('#chat-form').on('submit', function sent(event) {
+        event.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: $(this).serialize()
+        })
+            .done(
+                function (data) {
+                    $('.name-input').remove();
+                    $('.chat_input').val('');
+                    $('.msg_container_base').append(data);
 
-                var chatlist = document.getElementById('msg-list-div');
-                chatlist.scrollTop = chatlist.scrollHeight;
-                get_message()
-            });
-    return false
-});
+                    var chatlist = document.getElementById('msg-list-div');
+                    chatlist.scrollTop = chatlist.scrollHeight;
+                    get_message()
+                });
+        return false
+    });
+}
+
 
 function get_message() {
     $.ajax({
@@ -34,7 +47,7 @@ function get_message() {
             $('.msg_container_base').append(data);
             var chatlist = document.getElementById('msg-list-div');
             chatlist.scrollTop = chatlist.scrollHeight;
-            if(data){
+            if (data) {
                 read_message();
             }
         })
@@ -76,7 +89,6 @@ function read_message() {
 //             setTimeout(get_message_list, 6000)
 //         );
 // }
-
 
 
 // function seen_message() {
