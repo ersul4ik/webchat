c7a3354f6e9e65ff95b5794a15a83845ca12e6b4
ersul4ik <ersul4ik@mail.ru>
2017-12-02 18:53:29 +0600

    Fixed raven config

--- webchat/settings.py
+++ webchat/settings.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = ['*']
 LOGIN_URL = '/login/'
 
 RAVEN_CONFIG = {
-    'dsn': '<your-dsn-here>',
+    'dsn': 'https://04f8f2235e4240618582db8be0ee5b7f:cecb6460776f4b76be3aa09e62c8511c@sentry.io/252877',
 }
 
 LOGGING = {
